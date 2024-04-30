cb7f739f79688eb1ec566299bff82615443bccb1
andrewtroicki <atroicki@icloud.com>
2024-04-30 12:08:44 +1000

    tests passed

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('TheDOM has loaded');
+    console.log(document.querySelector('p'));
+    document.querySelector('p').textContent = 'This is really cool!'
+})
